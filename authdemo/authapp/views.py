6c4c72b97637a22ebe97955a5ee7a13b5c64794d
Fayaz409 <ftunio06@gmail.com>
2025-05-06 13:52:48 +0500

    Login View

--- authdemo/authapp/views.py
+++ authdemo/authapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login,authenticate
 # Create your views here.
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
@@ -19,3 +19,17 @@ def signup(request):
             login(request,user)
             return redirect('home')
     return render(request,'authapp/signup.html',{'form':form})
+
+def login_view(request):
+    if request.method=='POST':
+        form = AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+            return redirect('home')
+    else:
+      form = AuthenticationForm()
+    return render(request,'authapp/login.html',{'form':form}) 
