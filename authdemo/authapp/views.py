d808a29610568ce8c7e3dcddea3ad22c648726e9
Fayaz409 <ftunio06@gmail.com>
2025-05-06 13:19:05 +0500

    LogIn Required Decorator

--- authdemo/authapp/views.py
+++ authdemo/authapp/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 # Create your views here.
+from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
 
+
+@login_required
 def index(request):
     return render(request,'authapp/index.html')
 
