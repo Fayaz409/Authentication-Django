b059f0fe60feac89208ff2d80f8d37cf9d37f251
Fayaz409 <ftunio06@gmail.com>
2025-05-06 12:19:51 +0500

    SignUp View and Modified UsercreationForm

--- authdemo/authapp/views.py
+++ authdemo/authapp/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 # Create your views here.
+from .forms import SignUpForm
 
 def index(request):
     return render(request,'authapp/index.html')
+
+def signup(request):
+    form = SignUpForm()
+    if request.method=='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('home')
+    return render(request,'authapp/signup.html',{'form':form})
