92793c11861197486bf963ee1f53a6f1628e22c8
Fayaz409 <ftunio06@gmail.com>
2025-05-06 14:16:54 +0500

    Change Password Functionality

--- authdemo/authapp/urls.py
+++ authdemo/authapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns =[
    path('',views.index,name='home'),
    path('signup/',views.signup,name='signup'),
-   path('login',views.login_view,name='login')
+   path('login',views.login_view,name='login'),
+   path('change-password/',views.change_pasword,name='change-password'),
 ]
--- authdemo/authapp/views.py
+++ authdemo/authapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import login,authenticate
+from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm, UserCreationForm
+from django.contrib.auth import update_session_auth_hash,login,authenticate
 # Create your views here.
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
@@ -33,3 +33,15 @@ def login_view(request):
     else:
       form = AuthenticationForm()
     return render(request,'authapp/login.html',{'form':form}) 
+
+@login_required
+def change_pasword(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user,request.POST)
+        if form.is_valid():
+            user=form.save()
+            update_session_auth_hash(request,user)
+            return redirect('home')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request,'authapp/change_password.html',{'form':form})
